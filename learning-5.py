25e1393f553f037989f9a60b744eab6fcbf0a2f6
107598012 <linitachi@gmail.com>
2019-03-21 01:02:22 +0800

    no message

--- learning-5.py
+++ learning-5.py
@@ -3,7 +3,9 @@ import datetime
 import numpy as np
 import random
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 
 def createDataSet(dataset, fileName):
@@ -41,31 +43,54 @@ if __name__ == '__main__':
     dataset = []
     dataset = createDataSet(dataset, "machine.data")
     final = [0] * 13
+    r2Score = [0] * 13
     times = 10
+
     for time in range(times):
         k = 3
         trainset, testset = chose_seventity(dataset)
         trainset = np.array(trainset)
         testset = np.array(testset)
-        answer = trainset[:, 7]
-        TRAN = trainset[:, :7]
+        answer = trainset[:, 6]
+        TRAN = np.delete(trainset, np.s_[6:8], axis=1)
         mean = []
         for i in range(13):
             predict = np.array([])
             neigh = KNeighborsRegressor(n_neighbors=k)
             neigh.fit(TRAN, answer)
+            test = np.delete(testset, np.s_[6:8], axis=1)
             for j in range(len(testset)):
-                predict = np.append(predict, neigh.predict([testset[j, :7]]))
-            check = testset[:, 7]
-            error = (abs(predict-check))/check
-            mean.append((np.sum(error)/len(error)*100))
-            final[i] = final[i]+np.sum(error)/len(error)*100
+                predict = np.append(predict, neigh.predict([test[j, ]]))
+            check = testset[:, 6]
+            # 把極端值忽略
+            sub = predict-check
+            for q in range(5):
+                maxv = np.where(sub == np.max(sub))
+                sub = np.delete(sub, maxv)
+                predict = np.delete(predict, maxv)
+                check = np.delete(check, maxv)
+
+                minv = np.where(sub == np.min(sub))
+                sub = np.delete(sub, minv)
+                predict = np.delete(predict, minv)
+                check = np.delete(check, minv)
+
+            error = mean_squared_error(check, predict)
+            score = r2_score(check, predict)
+            r2Score[i] = r2Score[i]+score
+            final[i] = final[i]+error
             k = k+1
     final = [c/times for c in final]
+    r2Score = [c/times for c in r2Score]
     plt.xlabel('K')
-    plt.ylabel("Relative Error(%)")
+    plt.ylabel("Mean squared error ")
     plt.plot(range(3, 16), final)
     end = datetime.datetime.now()
     print(end - start)
+    plt.show()
+
+    plt.xlabel('K')
+    plt.ylabel("r2_score")
+    plt.plot(range(3, 16), r2Score)
     plt.show()
     print('end')
